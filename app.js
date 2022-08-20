67a62b171f144ad80b5d8a116c058b862721468a
Thomas Heck <dutchfence@gmail.com>
2022-08-20 18:25:06 +0200

    oAuth2 Google implemented
    Cleaned stylesheets
    Now working on Facebook oAuth2 and login/register screen

--- app.js
+++ app.js
@@ -1,20 +1,101 @@
+//Requires
+require("dotenv").config();
 const express = require("express");
+
+process.setMaxListeners(0);
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash= require("lodash");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+// Inits
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = 3000;
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user,done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/dashboard",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+// Variables
+
+
+
 app.get("/", function(req, res){
   res.render("home");
 });
 
+app.get("/login", function(req,res){
+  res.render("login");
+});
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
+  app.get('/auth/google/dashboard',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/dashboard');
+    });
 
-app.listen(3000, function(){
-  console.log("server started on port: ", port);
+app.get("/dashboard", function(req,res){
+  if(req.isAuthenticated()){
+    res.render("dashboard");
+
+}
+  else{
+    res.redirect("/login");
+  }
+});
+
+app.listen(process.env.PORT, function(){
+  console.log("server started on port: ", process.env.PORT);
 });

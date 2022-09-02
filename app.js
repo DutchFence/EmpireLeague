ad48b4b406f06608b16af9ae9c7597c885ba1f08
Thomas <dutchfence@gmail.com>
2022-09-02 14:18:23 +0200

    Register error handling done
    Now going to do login error handling

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require("express");
 process.setMaxListeners(0);
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash= require("lodash");
+const lodash = require("lodash");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
@@ -25,7 +25,9 @@ const options = {
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(session({
   secret: process.env.SECRET,
@@ -35,14 +37,36 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 const userSchema = new mongoose.Schema({
-  email: {type:String,required:true,unique:true},
-  email_verified:{type:Boolean,default:false},
-  username:{type:String, unique:true},
-  password: {type:String},
-  provider: {type:String, required:true, default:"Empire League"},
+  email: {
+    type: String,
+    required: true
+  },
+  email_verified: {
+    type: Boolean,
+    default: false
+  },
+  username: {
+    type: String,
+    required: false,
+    unique: true
+  },
+  display: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: false
+  },
+  provider: {
+    type: String,
+    required: true,
+    default: "Empire League"
+  },
   providerId: String,
   secret: String
 });
@@ -53,12 +77,12 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user,done){
+passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done){
-  User.findById(id, function(err, user){
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
     done(err, user);
   });
 });
@@ -67,11 +91,16 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/dashboard",
-    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    User.findOrCreate({email: profile.emails[0].value, provider:"Google", providerId: profile.id }, function (err, user) {
+    // console.log(profile);
+    User.findOrCreate({
+      email: profile.emails[0].value,
+      username: profile.emails[0].value,
+      provider: "Google",
+      providerId: profile.id
+    }, function(err, user) {
       return cb(err, user);
     });
   }
@@ -81,13 +110,18 @@ passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
     callbackURL: "http://localhost:3000/auth/facebook/dashboard",
-     enableProof: true,
+    enableProof: true,
     profileFields: ['id', 'emails']
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    User.findOrCreate({ email: profile.emails[0].value, provider: "Facebook", providerId: profile.id }, function (err, user) {
-      console.log(profile.id);
+    // console.log(profile);
+    User.findOrCreate({
+      email: profile.emails[0].value,
+      username: profile.emails[0].value,
+      provider: "Facebook",
+      providerId: profile.id
+    }, function(err, user) {
+      // console.log(profile.id);
       return cb(err, user);
     });
   }
@@ -96,95 +130,161 @@ passport.use(new FacebookStrategy({
 
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.post("/login", function(req,res){
-const user = new User({
-  username: req.body.username,
-  password: req.body.password
-});
-req.login(user, function(err){
-  if(err){
-    console.log(err);
-  }else{
-    passport.authenticate("local")(req,res, function(){
-      res.redirect("/dashboard");
-    });
-  };
-});
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/dashboard");
+      });
+    };
+  });
 });
 
-app.post('/logout', function(req, res){
+app.post('/logout', function(req, res) {
   req.logout(function(err) {
-    if (err) { return next(err); }
+    if (err) {
+      return next(err);
+    }
     res.redirect('/');
   });
 });
 
-app.get("/login", function(req,res){
-if(req.isAuthenticated()){
-  res.render("dashboard", {profile:req});
-}else{
-  res.render("login");
-}
+app.get("/login", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("dashboard", {
+      profile: req
+    });
+  } else {
+    res.render("login");
+  }
 });
-app.get('/verifyEmailRegistration', function(req, res){
+app.get('/verifyEmailRegistration', function(req, res) {
   res.render("verifyEmailRegistration");
 });
-app.post("/register", function(req, res){
-  User.register({email: req.body.email, username: req.body.username}, req.body.password, function(err, user){
-    if(err){
-      console.log(err);
-      res.redirect("/register");
-    }else{
-      passport.authenticate("local")(req,res,function(){
-        res.redirect("/verifyEmailRegistration");
+app.post("/register", function(req, res) {
+  console.log("post register");
+  User.register({
+    email: req.body.email,
+    username: req.body.email
+  }, req.body.password, function(err, user) {
+    console.log("registered");
+    if (req.body.password.length < 7) {
+      console.log("te kort");
+      let data = {
+        email: req.body.email,
+        content: "Password is too short: requires a minimum of 7 characters"
+      };
+      res.render("register", {
+        message: data
       });
+    } else if (req.body.password != req.body.verifyPassword) {
+      console.log("matching passwords");
+      let data = {
+        email: req.body.email,
+        content: "Passwords don't match: try again gl hf"
+      };
+      res.render("register", {
+        message: data
+      });
+    } else if (err) {
+      console.log("err");
+      let data = {
+        email: req.body.email,
+        content: err
+      };
+      res.render("register", {
+        message: data
+      });
+      console.log(err);
+    } else {
+      console.log("else");
+      console.log(req);
+
+      if (req.isAuthenticated()) {
+        console.log("authenticated ");
+        console.log(req);
+        res.render("dashboard", {
+          profile: req
+        });
+
+
+      } else {
+        console.log("lol");
+      }
+
     }
   });
+
 });
 
 app.get('/auth/facebook',
-  passport.authenticate('facebook',{ scope : ['email'] }));
+  passport.authenticate('facebook', {
+    scope: ['email']
+  }));
 
-  app.get('/auth/facebook/dashboard',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
-    function(req, res) {
+app.get('/auth/facebook/dashboard',
+  passport.authenticate('facebook', {
+    failureRedirect: '/login'
+  }),
+  function(req, res) {
 
-      // Successful authentication, redirect home.
-      res.redirect('/dashboard');
-    });
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
 
-  app.get('/auth/google/dashboard',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect('/dashboard');
+app.get('/auth/google/dashboard',
+  passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
+
+app.get("/register", function(req, res) {
+  console.log("get register");
+  let data = {
+    email: "",
+    content: "The longer the better and keep it original. Minimum of 7 characters."
+  }
+  res.render("register", {
+    message: data
+  });
+});
+
+
+app.get("/dashboard", function(req, res) {
+  // console.log(req);
+  console.log("redirected naar dashboard");
+  if (req.isAuthenticated()) {
+    console.log("authenticated ");
+    console.log(req);
+    res.render("dashboard", {
+      profile: req
     });
 
-    app.get("/register", function(req,res){
-      res.render("register");
-    });
-app.get("/dashboard", function(req,res){
-  console.log(req);
-  if(req.isAuthenticated()){
 
-
-    res.render("dashboard",{profile:req});
-
-
-}
-  else{
+  } else {
     res.redirect("/login");
   }
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT, function() {
   console.log("server started on port: ", process.env.PORT);
 });
 // https.createServer(options, app).listen(8080, () => {
